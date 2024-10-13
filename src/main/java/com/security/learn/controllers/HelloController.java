8a5381c9e2f6fa394ab43ee9ecc8e4fbd1c16667
Nithin Titus <nithin22796@gmail.com>
2024-10-13 11:52:09 -0500

    Role Based Authorization (@PreAuthorize)

--- src/main/java/com/security/learn/controllers/HelloController.java
+++ src/main/java/com/security/learn/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.security.learn.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class HelloController {
     @GetMapping("/")
     public String greet() {
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String greet1() {
+        return "Hello User !";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String greet2() {
+        return "Hello Admin !";
     }
 }
