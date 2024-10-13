e52ba5612c4f18e0e1a3b529f5a58a0ce40a7823
Nithin Titus <nithin22796@gmail.com>
2024-10-13 12:45:00 -0500

    H1 Database configuration

--- src/main/java/com/security/learn/configuration/SecurityConfig.java
+++ src/main/java/com/security/learn/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.security.learn.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,25 +8,36 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .build();
     }
 
@@ -43,7 +55,17 @@ public class SecurityConfig {
                 .password("{noop}password")
                 .roles("ADMIN")
                 .build();
+        /*
+        *  In Memory manager
+           return new InMemoryUserDetailsManager(user1, user2);
+        */
 
-        return new InMemoryUserDetailsManager(user1, user2);
+        /*
+        * JDBC - H2 Database Manager
+        */
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+        manager.createUser(user1);
+        manager.createUser(user2);
+        return manager;
     }
 }
